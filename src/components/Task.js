de6d9bdaf03f0bdd7e097e249c44baf7cb56c533
Thomas Farrell <44026261+TF20083540@users.noreply.github.com>
2023-10-02 12:18:41 +0100

    Week 3 Lab A done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,16 @@
 import Rect from 'react';
 
+
 const Task = (props) => {
     
     return (
-        <div className="card">
+        
+        <div className="card" style={{backgroundColor:props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.description}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
             <p>{props.children}</p>
         </div>
     )
